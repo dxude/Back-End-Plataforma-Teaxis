e13ac3a25e37c6f83bf851e4ad782f1f038ebab0
PanmelaKaren <161941633+PanmelaKaren@users.noreply.github.com>
2025-11-25 18:22:29 +0000

    Adicionando getters e setters na entidade Meta

--- src/main/java/br/com/teaxis/api/model/Meta.java
+++ src/main/java/br/com/teaxis/api/model/Meta.java
@@ -31,6 +31,44 @@ public class Meta {
         this.status = status;
     }
 
-    // Getters e Setters ...
+    public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public Long getIdUsuario() {
+    return idUsuario;
+}
+
+public void setIdUsuario(Long idUsuario) {
+    this.idUsuario = idUsuario;
+}
+
+public String getTitulo() {
+    return titulo;
+}
+
+public void setTitulo(String titulo) {
+    this.titulo = titulo;
+}
+
+public String getDescricao() {
+    return descricao;
+}
+
+public void setDescricao(String descricao) {
+    this.descricao = descricao;
+}
+
+public String getStatus() {
+    return status;
+}
+
+public void setStatus(String status) {
+    this.status = status;
+}
 
 }
