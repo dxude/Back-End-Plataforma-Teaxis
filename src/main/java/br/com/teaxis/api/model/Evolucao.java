3e5fb7727cfb0db16925502afdbe1ced926ef1c3
AndrezaCarrilho <108102040+AndrezaCarrilho@users.noreply.github.com>
2025-11-26 13:48:41 +0000

    Refatorando para usar Lombok e corrigir duplicidade

--- src/main/java/br/com/teaxis/api/model/Evolucao.java
+++ src/main/java/br/com/teaxis/api/model/Evolucao.java
@@ -1,8 +1,10 @@
 package br.com.teaxis.api.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import java.time.LocalDate;
 
+@Data
 @Entity
 @Table(name = "evolucoes")
 public class Evolucao {
@@ -22,54 +24,4 @@ public class Evolucao {
 
     @Column(columnDefinition = "TEXT")
     private String comentario;
-
-    public Evolucao() {
-    }
-
-    public Evolucao(Meta meta, LocalDate dataRegistro, Double progresso, String comentario) {
-        this.meta = meta;
-        this.dataRegistro = dataRegistro;
-        this.progresso = progresso;
-        this.comentario = comentario;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Meta getMeta() {
-        return meta;
-    }
-
-    public void setMeta(Meta meta) {
-        this.meta = meta;
-    }
-
-    public LocalDate getDataRegistro() {
-        return dataRegistro;
-    }
-
-    public void setDataRegistro(LocalDate dataRegistro) {
-        this.dataRegistro = dataRegistro;
-    }
-
-    public Double getProgresso() {
-        return progresso;
-    }
-
-    public void setProgresso(Double progresso) {
-        this.progresso = progresso;
-    }
-
-    public String getComentario() {
-        return comentario;
-    }
-
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
-    }
 }
